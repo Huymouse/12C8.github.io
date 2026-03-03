eb5e419a4e397d958bb3a079248ed6ffe5f07fba
Huymouse <93029514+Huymouse@users.noreply.github.com>
2026-03-03 11:58:23 +0000

    côm mít

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Danh sách 12 phần thưởng
 const gifts = [
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" },
-    { name: "Mạnh Tân", image: "manh-tan.jpg" }
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" },
+    { name: "Mạnh Tân", image: "manh-tan.png" }
 ];
 
 const colors = ['c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10', 'c11', 'c12'];
